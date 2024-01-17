a361465d908ce1a8385f25ebb31031001e254345
Timothy Small <smalls.online@icloud.com>
2024-01-17 15:54:22 -0500

    Add new constructor

--- src/Lib/Services/QueueClientService/QueueClientService.cs
+++ src/Lib/Services/QueueClientService/QueueClientService.cs
@@ -1,3 +1,4 @@
+using Azure.Core;
 using Azure.Storage.Queues;
 
 namespace EntraMfaPrefillinator.Lib.Services;
@@ -11,6 +12,19 @@ public partial class QueueClientService : IQueueClientService
         _authUpdateQueueClient = new(
             connectionString: connectionString,
             queueName: "authupdate-queue",
+            options: new()
+            {
+                MessageEncoding = QueueMessageEncoding.Base64
+            }
+        );
+        _authUpdateQueueClient.CreateIfNotExists();
+    }
+
+    public QueueClientService(Uri queueUri, TokenCredential tokenCredential)
+    {
+        _authUpdateQueueClient = new(
+            queueUri: queueUri,
+            credential: tokenCredential,
             options: new()
             {
                 MessageEncoding = QueueMessageEncoding.Base64
