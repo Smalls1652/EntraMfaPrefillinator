85f67ccfd5ee1c0432112a0d8d6eaee58f7ac066
Timothy Small <smalls.online@icloud.com>
2024-02-13 12:45:25 -0500

    Update IGraphClientService

--- src/Lib/Services/interfaces/IGraphClientService.cs
+++ src/Lib/Services/interfaces/IGraphClientService.cs
@@ -5,9 +5,20 @@ namespace EntraMfaPrefillinator.Lib.Services;
 public interface IGraphClientService
 {
     Task<User> GetUserAsync(string userId);
+    Task<User> GetUserAsync(string userId, string? parentActivityId);
+
     Task<User> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber);
+    Task<User> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber, string? parentActivityId);
+
     Task<PhoneAuthenticationMethod> AddPhoneAuthenticationMethodAsync(string userId, string phoneNumber);
+    Task<PhoneAuthenticationMethod> AddPhoneAuthenticationMethodAsync(string userId, string phoneNumber, string? parentActivityId);
+
     Task<EmailAuthenticationMethod> AddEmailAuthenticationMethodAsync(string userId, string emailAddress);
+    Task<EmailAuthenticationMethod> AddEmailAuthenticationMethodAsync(string userId, string emailAddress, string? parentActivityId);
+
     Task<PhoneAuthenticationMethod[]?> GetPhoneAuthenticationMethodsAsync(string userId);
+    Task<PhoneAuthenticationMethod[]?> GetPhoneAuthenticationMethodsAsync(string userId, string? parentActivityId);
+
     Task<EmailAuthenticationMethod[]?> GetEmailAuthenticationMethodsAsync(string userId);
+    Task<EmailAuthenticationMethod[]?> GetEmailAuthenticationMethodsAsync(string userId, string? parentActivityId);
 }
