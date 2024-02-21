8958d39c5ef241b780145dd300931c0caca689bd
Timothy Small <smalls.online@icloud.com>
2024-02-21 12:53:41 -0500

    Update error handling for API calls
    - User results are now nullable.
    - Enhance error handling for JSON deserialization.

--- src/Lib/Services/interfaces/IGraphClientService.cs
+++ src/Lib/Services/interfaces/IGraphClientService.cs
@@ -4,11 +4,11 @@ namespace EntraMfaPrefillinator.Lib.Services;
 
 public interface IGraphClientService
 {
-    Task<User> GetUserAsync(string userId);
-    Task<User> GetUserAsync(string userId, string? parentActivityId);
+    Task<User?> GetUserAsync(string userId);
+    Task<User?> GetUserAsync(string userId, string? parentActivityId);
 
-    Task<User> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber);
-    Task<User> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber, string? parentActivityId);
+    Task<User?> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber);
+    Task<User?> GetUserByUserNameAndEmployeeNumberAsync(string? userName, string? employeeNumber, string? parentActivityId);
 
     Task<PhoneAuthenticationMethod> AddPhoneAuthenticationMethodAsync(string userId, string phoneNumber);
     Task<PhoneAuthenticationMethod> AddPhoneAuthenticationMethodAsync(string userId, string phoneNumber, string? parentActivityId);
