fdf4651776638cb9cc70d19f89c29f15cdd83523
Timothy Small <smalls.online@icloud.com>
2024-04-12 14:32:06 -0400

    Add code docs to UserDetailsDbContext

--- src/Tools/CsvImporter.Database/Contexts/UserDetailsDbContext.cs
+++ src/Tools/CsvImporter.Database/Contexts/UserDetailsDbContext.cs
@@ -4,10 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EntraMfaPrefillinator.Tools.CsvImporter.Database.Contexts;
 
+/// <summary>
+/// Database context for <see cref="UserDetails"/>.
+/// </summary>
 public class UserDetailsDbContext : DbContext
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserDetailsDbContext"/> class.
+    /// </summary>
+    /// <param name="options">The database context options.</param>
     public UserDetailsDbContext(DbContextOptions<UserDetailsDbContext> options) : base(options)
     {}
 
+    /// <summary>
+    /// Items in the 'UserDetails' table.
+    /// </summary>
     public DbSet<UserDetails> UserDetails { get; set; } = null!;
 }
